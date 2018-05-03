722af5002c82ab0d663c84ec77237e0d91c2372c
Simone Contini <s.contini76@gmail.com>
2018-05-04 00:21:28 +0200

    import iniparse 0.3.2

--- tests/test_fuzz.py
+++ tests/test_fuzz.py
@@ -1,8 +1,8 @@
 import re
+import os
 import random
 import unittest
 import ConfigParser
-from textwrap import dedent
 from StringIO import StringIO
 from iniparse import compat, ini
 
@@ -69,35 +69,58 @@ def random_ini_file():
 class test_fuzz(unittest.TestCase):
     def test_fuzz(self):
         random.seed(42)
-        for i in range(100):
-            # parse random file with errors disabled
-            s = random_ini_file()
-            c = ini.INIConfig(parse_exc=False)
-            c._readfp(StringIO(s))
-            # check that file is preserved, except for
-            # commenting out erroneous lines
-            l1 = s.split('\n')
-            l2 = str(c).split('\n')
-            self.assertEqual(len(l1), len(l2))
-            good_lines = []
-            for i in range(len(l1)):
-                try:
-                    self.assertEqual(l1[i], l2[i])
-                    good_lines.append(l1[i])
-                except AssertionError:
-                    self.assertEqual('#'+l1[i], l2[i])
-            # parse the good subset of the file
-            # using ConfigParser
-            s = '\n'.join(good_lines)
-            cc = compat.RawConfigParser()
-            cc.readfp(StringIO(s))
-            cc_py = ConfigParser.RawConfigParser()
-            cc_py.readfp(StringIO(s))
-            # compare the two configparsers
-            self.assertEqualSorted(cc_py.sections(), cc.sections())
-            self.assertEqualSorted(cc_py.defaults().items(), cc.defaults().items())
-            for sec in cc_py.sections():
-                self.assertEqualSorted(cc_py.items(sec), cc.items(sec))
+        try:
+            num_iter = int(os.environ['INIPARSE_FUZZ_ITERATIONS'])
+        except (KeyError, ValueError):
+            num_iter = 100
+        for fuzz_iter in range(num_iter):
+            try:
+                # parse random file with errors disabled
+                s = random_ini_file()
+                c = ini.INIConfig(parse_exc=False)
+                c._readfp(StringIO(s))
+                # check that file is preserved, except for
+                # commenting out erroneous lines
+                l1 = s.split('\n')
+                l2 = str(c).split('\n')
+                self.assertEqual(len(l1), len(l2))
+                good_lines = []
+                for i in range(len(l1)):
+                    try:
+                        self.assertEqual(l1[i], l2[i])
+                        good_lines.append(l1[i])
+                    except AssertionError:
+                        self.assertEqual('#'+l1[i], l2[i])
+                # parse the good subset of the file
+                # using ConfigParser
+                s = '\n'.join(good_lines)
+                cc = compat.RawConfigParser()
+                cc.readfp(StringIO(s))
+                cc_py = ConfigParser.RawConfigParser()
+                cc_py.readfp(StringIO(s))
+                # compare the two configparsers
+                self.assertEqualConfig(cc_py, cc)
+                # check that tidy does not change semantics
+                ini.tidy(cc)
+                cc_tidy = ConfigParser.RawConfigParser()
+                cc_tidy.readfp(StringIO(str(cc.data)))
+                self.assertEqualConfig(cc_py, cc_tidy)
+            except AssertionError:
+                fname = 'fuzz-test-iter-%d.ini' % fuzz_iter
+                print 'Fuzz test failed at iteration', fuzz_iter
+                print 'Writing out failing INI file as', fname
+                f = open(fname, 'w')
+                f.write(s)
+                f.close()
+                raise
+
+    def assertEqualConfig(self, c1, c2):
+        self.assertEqualSorted(c1.sections(), c2.sections())
+        self.assertEqualSorted(c1.defaults().items(), c2.defaults().items())
+        for sec in c1.sections():
+            self.assertEqualSorted(c1.options(sec), c2.options(sec))
+            for opt in c1.options(sec):
+                self.assertEqual(c1.get(sec, opt), c2.get(sec, opt))
 
     def assertEqualSorted(self, l1, l2):
         l1.sort()
